de6fb5d5e066b890f3e53088cbd4296a446b8b17
ThomasPootQA <thomas.poot@qa.com>
2019-01-24 14:29:42 +0000

    Removed unused import

--- src/main/java/uk/gov/ons/fwmt/census/tm/mock/controller/MockLoggerController.java
+++ src/main/java/uk/gov/ons/fwmt/census/tm/mock/controller/MockLoggerController.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpServerErrorException;
 import uk.gov.ons.fwmt.census.tm.mock.logging.MockLogger;
 import uk.gov.ons.fwmt.census.tm.mock.logging.MockMessage;
 
