a9f591eb3e986e64328ff4be0abd08bb2394180a
ThomasPoot <35296806+thomaspoot@users.noreply.github.com>
2020-04-25 15:35:29 +0100

    Updated the mock to use a new version of common, and fit TM's current spec. Please use the updated common (same branch name) for local testing. PR/integrate the new version of common FIRST.

--- src/test/java/uk/gov/ons/census/fwmt/tm/mock/tm/comet/api/CasesApiControllerIntegrationTest.java
+++ src/test/java/uk/gov/ons/census/fwmt/tm/mock/tm/comet/api/CasesApiControllerIntegrationTest.java
@@ -8,10 +8,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-import uk.gov.ons.census.fwmt.common.data.modelcase.CasePause;
-import uk.gov.ons.census.fwmt.common.data.modelcase.CasePauseRequest;
-import uk.gov.ons.census.fwmt.common.data.modelcase.CaseCreateRequest;
-import uk.gov.ons.census.fwmt.common.data.modelcase.ModelCase;
+import uk.gov.ons.census.fwmt.common.data.tm.Case;
+import uk.gov.ons.census.fwmt.common.data.tm.CasePause;
+import uk.gov.ons.census.fwmt.common.data.tm.CasePauseRequest;
+import uk.gov.ons.census.fwmt.common.data.tm.CaseRequest;
 import uk.gov.ons.census.fwmt.tm.mock.comet.api.CasesApi;
 
 import static org.junit.Assert.assertEquals;
@@ -27,29 +27,29 @@ public class CasesApiControllerIntegrationTest {
   @Test
   public void casesByIdGetTest() {
     String id = "id_example";
-    ResponseEntity<ModelCase> responseEntity = casesApi.casesByIdGet(id);
+    ResponseEntity<Case> responseEntity = casesApi.getCase(id, null);
     assertEquals(HttpStatus.NOT_IMPLEMENTED, responseEntity.getStatusCode());
   }
 
   @Test
   public void casesByIdPostTest() {
     String id = "id_example";
-    CaseCreateRequest body = new CaseCreateRequest();
-    ResponseEntity<ModelCase> responseEntity = casesApi.casesByIdPut(id, body);
+    CaseRequest body = new CaseRequest();
+    ResponseEntity<Case> responseEntity = casesApi.putCase(id, body);
     assertEquals(HttpStatus.NOT_IMPLEMENTED, responseEntity.getStatusCode());
   }
 
   @Test
   public void casesByIdPauseDeleteTest() {
     String id = "id_example";
-    ResponseEntity<Void> responseEntity = casesApi.casesByIdPauseDelete(id);
+    ResponseEntity<Void> responseEntity = casesApi.deleteCasePause(id);
     assertEquals(HttpStatus.NOT_IMPLEMENTED, responseEntity.getStatusCode());
   }
 
   @Test
   public void casesByIdPauseGetTest() {
     String id = "id_example";
-    ResponseEntity<CasePause> responseEntity = casesApi.casesByIdPauseGet(id);
+    ResponseEntity<CasePause> responseEntity = casesApi.getCasePause(id);
     assertEquals(HttpStatus.NOT_IMPLEMENTED, responseEntity.getStatusCode());
   }
 
@@ -57,15 +57,15 @@ public class CasesApiControllerIntegrationTest {
   public void casesByIdPausePutTest() {
     String id = "id_example";
     CasePauseRequest body = new CasePauseRequest();
-    ResponseEntity<CasePause> responseEntity = casesApi.casesByIdPausePut(id, body);
+    ResponseEntity<CasePause> responseEntity = casesApi.putCasePause(id, body);
     assertEquals(HttpStatus.NOT_IMPLEMENTED, responseEntity.getStatusCode());
   }
 
   @Test
   public void casesByIdPutTest() {
     String id = "id_example";
-    CaseCreateRequest body = new CaseCreateRequest();
-    ResponseEntity<ModelCase> responseEntity = casesApi.casesByIdPut(id, body);
+    CaseRequest body = new CaseRequest();
+    ResponseEntity<Case> responseEntity = casesApi.putCase(id, body);
     assertEquals(HttpStatus.NOT_IMPLEMENTED, responseEntity.getStatusCode());
   }
 }
